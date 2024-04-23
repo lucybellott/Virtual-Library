4817b6c58844efd4d060d604985a0b3ea7286b77
Lucy Bellott <lbellott@tripadvisor.com>
2024-04-23 17:29:43 -0400

    media.js

--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,33 @@
 // create your Media class:
 
-// uncomment below to export it:
+// In Media.js, create a Media class. The Media class should have the following properties and methods:
+// title: the title of the media (string)
+// year: the year the media was produced (number)
+// genre: the genre of the media (string)
+// totalMediaCount: A static property that tracks how many Media items have been created
+// summary(): a method that returns a summary of the media (string) as "Title: <TITTLE>, Year: <YEAR>, Genre: <GENRE>"
+// Export the Media class using module.exports.
+// In index.js, import Media using require.
+
+
+
+class Media {
+
+ static totalMediaCount = 0;
+    
+   constructor(title, year, genre){
+        this.title = title
+        this.year = year
+        this.genre = genre
+        Media.totalMediaCount++
+    }
+   
+
+    summary(){
+        return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre}`
+    }
+    
+}
+
+
 module.exports = Media;
